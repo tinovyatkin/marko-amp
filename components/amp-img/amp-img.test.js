299f377c45d7b9b2fa3fe3776a93dda4c21d1004
Patrick Steele-Idem <pnidem@gmail.com>
2017-09-06 07:38:48 -0600

    Moved all HTML rendering into the .marko file and other cleanup

--- components/amp-img/amp-img.test.js
+++ components/amp-img/amp-img.test.js
@@ -1,108 +1,126 @@
 'use strict';
 
 /* eslint-env browser */
-const marko = require('marko');
-const path = require('path');
 
-const ampImgRenderer = require('./amp-img');
+const AmpImg = require('marko').load(require.resolve('./index.marko'));
 
 describe('amp-img', () => {
-  test('amp-img render', () => {
-    const tag = ampImgRenderer({
-      src: 'https://localhost',
-      width: 170,
-      height: 200,
-      alt: 'Test alt',
-    });
-    document.body.innerHTML = tag;
-    const tags = document.getElementsByTagName('amp-img');
-    expect(tags).toHaveLength(1);
-    const ampImg = tags[0];
-    expect(ampImg.getAttribute('src')).toBe('https://localhost');
-    expect(ampImg.getAttribute('width')).toBe('170');
-    expect(ampImg.getAttribute('height')).toBe('200');
-    expect(ampImg.getAttribute('alt')).toBe('Test alt');
+  test('render <amp-img> when out.global.amp is true', () => {
+    document.body.innerHTML = AmpImg.renderToString({
+        $global: {
+          amp: true
+        },
+        src: 'https://localhost',
+        width: 170,
+        height: 200,
+        alt: 'Test alt',
+      });
+
+    const els = document.getElementsByTagName('amp-img');
+    expect(els).toHaveLength(1);
+    const ampImgEl = els[0];
+    expect(ampImgEl.getAttribute('src')).toBe('https://localhost');
+    expect(ampImgEl.getAttribute('width')).toBe('170');
+    expect(ampImgEl.getAttribute('height')).toBe('200');
+    expect(ampImgEl.getAttribute('alt')).toBe('Test alt');
   });
 
-  test('Render component', async () => {
-    // loading template
-    const template = marko.load(path.join(__dirname, 'index.marko'));
-    // render normal version
-    const html = (await template.render({
-      src: 'https://image.url',
-      height: 170,
-      width: 170,
-      alt: 'Long long alt text',
-    })).toString();
-    expect(typeof html).toBe('string');
-    document.body.innerHTML = html;
-    const tags = document.getElementsByTagName('img');
-    expect(tags).toHaveLength(1);
-    const img = tags[0];
-    expect(img.getAttribute('src')).toBe('https://image.url');
-    expect(img.getAttribute('width')).toBe('170');
-    expect(img.getAttribute('height')).toBe('170');
-    expect(img.getAttribute('alt')).toBe('Long long alt text');
-    // render AMP version
-    const htmlAmp = (await template.render({
-      src: 'https://image.url',
-      height: 170,
-      width: 170,
-      alt: 'Long long alt text',
-      $global: {
-        amp: true,
-      },
-    })).toString();
-    expect(typeof htmlAmp).toBe('string');
-    document.body.innerHTML = htmlAmp;
-    const ampTags = document.getElementsByTagName('amp-img');
-    expect(ampTags).toHaveLength(1);
-    const ampImg = ampTags[0];
-    expect(ampImg.getAttribute('src')).toBe('https://image.url');
-    expect(ampImg.getAttribute('width')).toBe('170');
-    expect(ampImg.getAttribute('height')).toBe('170');
-    expect(ampImg.getAttribute('alt')).toBe('Long long alt text');
+  test('render <img> when out.global.amp is not true', () => {
+    document.body.innerHTML = AmpImg.renderToString({
+        src: 'https://localhost',
+        width: 170,
+        height: 200,
+        alt: 'Test alt',
+      });
+
+    const els = document.getElementsByTagName('img');
+    expect(els).toHaveLength(1);
+    const imgEl = els[0];
+    expect(imgEl.getAttribute('src')).toBe('https://localhost');
+    expect(imgEl.getAttribute('width')).toBe('170');
+    expect(imgEl.getAttribute('height')).toBe('200');
+    expect(imgEl.getAttribute('alt')).toBe('Test alt');
   });
 
-  test('Nested template HTML render', async () => {
-    // loading template
-    const template = marko.load(
-      path.join(__dirname, '../../__tests__/templates/amp-img-nested.marko'),
-    );
-    // render normal version
-    const html = (await template.render()).toString();
-    expect(typeof html).toBe('string');
-    document.body.innerHTML = html;
-    const tags = document.getElementsByTagName('picture');
-    console.log(html);
-    expect(tags).toHaveLength(1);
-    const picture = tags[0];
-    expect(picture.childElementCount).toBe(2);
-    const source = picture.firstElementChild;
-    expect(source.type).toBe('image/webp');
-    expect(source.srcset).toBe('images/mountains.webp');
-    const img = picture.lastElementChild;
-    expect(img.src).toBe('images/mountains.jpg');
-    expect(img.width).toBe(550);
-    expect(img.height).toBe(368);
-    expect(img.alt).toBe('Mountains');
-  });
-
-  test('Nested template AMP render', async () => {
-    // loading template
-    const template = marko.load(
-      path.join(__dirname, '../../__tests__/templates/amp-img-nested.marko'),
-    );
-    // render AMP version
-    const html = (await template.render({
-      $global: {
-        amp: true,
-      },
-    })).toString();
-    expect(typeof html).toBe('string');
-    document.body.innerHTML = html;
-    const tags = document.getElementsByTagName('amp-img');
-    console.log(html);
-    expect(tags).toHaveLength(2);
-  });
+  // test('Render component', async () => {
+  //   // loading template
+  //   const template = marko.load(path.join(__dirname, 'index.marko'));
+  //   // render normal version
+  //   const html = (await template.render({
+  //     src: 'https://image.url',
+  //     height: 170,
+  //     width: 170,
+  //     alt: 'Long long alt text',
+  //   })).toString();
+  //   expect(typeof html).toBe('string');
+  //   document.body.innerHTML = html;
+  //   const tags = document.getElementsByTagName('img');
+  //   expect(tags).toHaveLength(1);
+  //   const img = tags[0];
+  //   expect(img.getAttribute('src')).toBe('https://image.url');
+  //   expect(img.getAttribute('width')).toBe('170');
+  //   expect(img.getAttribute('height')).toBe('170');
+  //   expect(img.getAttribute('alt')).toBe('Long long alt text');
+  //   // render AMP version
+  //   const htmlAmp = (await template.render({
+  //     src: 'https://image.url',
+  //     height: 170,
+  //     width: 170,
+  //     alt: 'Long long alt text',
+  //     $global: {
+  //       amp: true,
+  //     },
+  //   })).toString();
+  //   expect(typeof htmlAmp).toBe('string');
+  //   document.body.innerHTML = htmlAmp;
+  //   const ampTags = document.getElementsByTagName('amp-img');
+  //   expect(ampTags).toHaveLength(1);
+  //   const ampImg = ampTags[0];
+  //   expect(ampImg.getAttribute('src')).toBe('https://image.url');
+  //   expect(ampImg.getAttribute('width')).toBe('170');
+  //   expect(ampImg.getAttribute('height')).toBe('170');
+  //   expect(ampImg.getAttribute('alt')).toBe('Long long alt text');
+  // });
+  //
+  // test('Nested template HTML render', async () => {
+  //   // loading template
+  //   const template = marko.load(
+  //     path.join(__dirname, '../../__tests__/templates/amp-img-nested.marko'),
+  //   );
+  //   // render normal version
+  //   const html = (await template.render()).toString();
+  //   expect(typeof html).toBe('string');
+  //   document.body.innerHTML = html;
+  //   const tags = document.getElementsByTagName('picture');
+  //   console.log(html);
+  //   expect(tags).toHaveLength(1);
+  //   const picture = tags[0];
+  //   expect(picture.childElementCount).toBe(2);
+  //   const source = picture.firstElementChild;
+  //   expect(source.type).toBe('image/webp');
+  //   expect(source.srcset).toBe('images/mountains.webp');
+  //   const img = picture.lastElementChild;
+  //   expect(img.src).toBe('images/mountains.jpg');
+  //   expect(img.width).toBe(550);
+  //   expect(img.height).toBe(368);
+  //   expect(img.alt).toBe('Mountains');
+  // });
+  //
+  // test('Nested template AMP render', async () => {
+  //   // loading template
+  //   const template = marko.load(
+  //     path.join(__dirname, '../../__tests__/templates/amp-img-nested.marko'),
+  //   );
+  //   // render AMP version
+  //   const html = (await template.render({
+  //     $global: {
+  //       amp: true,
+  //     },
+  //   })).toString();
+  //   expect(typeof html).toBe('string');
+  //   document.body.innerHTML = html;
+  //   const tags = document.getElementsByTagName('amp-img');
+  //   console.log(html);
+  //   expect(tags).toHaveLength(2);
+  // });
 });
--- components/amp-img/index.marko.js
+++ components/amp-img/index.marko.js
@@ -2,41 +2,94 @@
 "use strict";
 
 var marko_template = module.exports = require("marko/dist/html").t(__filename),
-    module_ampImgRenderer = require("./amp-img"),
-    ampImgRenderer = module_ampImgRenderer.default || module_ampImgRenderer,
     marko_helpers = require("marko/dist/runtime/html/helpers"),
-    marko_str = marko_helpers.s,
     marko_loadTag = marko_helpers.t,
     include_tag = marko_loadTag(require("marko/dist/taglibs/core/include-tag")),
+    marko_attrs = marko_helpers.as,
     marko_attr = marko_helpers.a,
     marko_classAttr = marko_helpers.ca;
+
+const ampAttrs = {
+  // <amp-img> attrs:
+  'src': true,
+  'srcset': true,
+  'sizes': true,
+  'alt': true,
+  'attribution': true,
+  'height': true,
+  'width': true,
+
+  // Common AMP attributes:
+  'fallback': true,
+  'heights': true,
+  'layout': true,
+  'media': true,
+  'noloading': true,
+  'on': true,
+  'placeholder': true,
+};
+// We just need to list out the attributes that are common with
+// the <amg-img> attributes since unrecognized attributes will
+// passthrough to the <img> tag
+const imgAttrs = {
+  'src': true,
+  'srcset': true,
+  'alt': true,
+  'height': true,
+  'width': true
+}
+
+function getAmpAttrs(input) {
+  return Object.keys(input).reduce((attrs, attrName) => {
+    if (attrName in ampAttrs) {
+      attrs[attrName] = input[attrName];
+    }
+    return attrs;
+  }, {});
+}
+
+function getImgAttrs(input) {
+  return Object.keys(input).reduce((attrs, attrName) => {
+    if (attrName in imgAttrs || !(attrName in ampAttrs)) {
+      attrs[attrName] = input[attrName];
+    }
+    return attrs;
+  }, {});
+};
 
 function render(input, out) {
   var data = input;
 
-  const { src, srcset, sizes, alt = "", height, width, layout, heights, media, class: classList } = input;
+  const { src, srcset } = input;
 
   const type = /\.webp$/i.test(src) ? "image/webp" : undefined;
 
   if (out.global.amp) {
-    out.w(marko_str(ampImgRenderer(input, !(!input.renderBody))));
+    if (!src && !srcset)
+      throw new Error(
+        `Either src or srcset attribute is required for AMP-IMG tag`,
+      );
+
+    out.w("<AMP-IMG" +
+      marko_attrs(getAmpAttrs(input)) +
+      ">");
 
     if (input.renderBody) {
       include_tag({
           _target: input
         }, out);
-
-      out.w("</amp-img>");
     }
+
+    out.w("</AMP-IMG>");
   } else {
     if (input.renderBody) {
       out.w("<picture" +
-        marko_classAttr(classList) +
+        marko_classAttr(input.class) +
         "><source" +
         marko_attr("type", type) +
         marko_attr("srcset", srcset || src) +
-        marko_attr("sizes", sizes) +
-        marko_attr("media", media) +
+        marko_attr("sizes", input.sizes) +
+        marko_attr("media", input.media) +
         ">");
 
       include_tag({
@@ -46,13 +99,7 @@ function render(input, out) {
       out.w("</picture>");
     } else {
       out.w("<img" +
-        marko_attr("src", src) +
-        marko_attr("srcset", srcset) +
-        marko_attr("sizes", sizes) +
-        marko_attr("alt", alt) +
-        marko_attr("height", height) +
-        marko_attr("width", width) +
-        marko_classAttr(classList) +
+        marko_attrs(getImgAttrs(input)) +
         ">");
     }
   }
