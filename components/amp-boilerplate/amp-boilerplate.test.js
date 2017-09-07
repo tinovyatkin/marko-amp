b1e49cc1ba3a47cd160570cb5635161b82018f90
tinovyatkin <tino@vtkn.io>
2017-09-07 13:23:15 +0300

    Implemented AMP-BOILERPLATE

---
+++ components/amp-boilerplate/amp-boilerplate.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/* eslint-env browser */
+const AmpBoilerplate = require('marko').load(require.resolve('./index.marko'));
+const AmpBoilerplateScripts = require('marko').load(
+  require.resolve('../../__tests__/templates/amp-boilerplate-scripts.marko'),
+);
+
+describe('amp-boilerplate', () => {
+  test('basic AMP rendering', async () => {
+    // Can't use sync rendering here, due to asyncFragment
+    document.head.innerHTML = (await AmpBoilerplate.render({
+      $global: {
+        amp: true,
+      },
+    })).toString();
+
+    expect(document.querySelectorAll('style[amp-boilerplate]')).toHaveLength(1);
+    expect(document.querySelectorAll('noscript')).toHaveLength(1);
+    expect(document.querySelector('script[async]').src).toBe(
+      'https://cdn.ampproject.org/v0.js',
+    );
+  });
+
+  test('empty HTML rendering', () => {
+    expect(AmpBoilerplate.renderToString()).toHaveLength(0);
+  });
+
+  test('additional AMP scripts collecting', async () => {
+    // Can't use sync rendering here, due to asyncFragment
+    document.documentElement.innerHTML = (await AmpBoilerplateScripts.render({
+      $global: {
+        amp: true,
+      },
+    })).toString();
+
+    expect(
+      document.head.querySelectorAll('style[amp-boilerplate]'),
+    ).toHaveLength(1);
+    expect(document.querySelectorAll('noscript')).toHaveLength(1);
+    const scripts = document.head.querySelectorAll('script[async]');
+    expect(scripts[0].src).toBe('https://cdn.ampproject.org/v0.js');
+    const additionalScript = scripts[1];
+    expect(additionalScript).toBeDefined();
+    expect(additionalScript.getAttribute('custom-element')).toBe('amp-test');
+    expect(additionalScript.src).toBe('https://test.url/');
+  });
+});
