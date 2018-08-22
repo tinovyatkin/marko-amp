e3adca6de033707cb67efe1b211faab336b73e9d
Tino Vyatkin <tino@vtkn.io>
2018-08-22 10:15:06 -0400

    add support for playsinline

--- components/amp-video/amp-video.js
+++ components/amp-video/amp-video.js
@@ -11,7 +11,7 @@ function renderAmpVideo(input = {}, dontClose) {
 
   // getting attributes names
   for (const attr in tagData.attributes) {
-    if (attr in input) {
+    if (attr in input && !/^playsinline$/i.test(attr)) {
       if (typeof input[attr] === 'boolean' || input[attr].length === 0)
         attributes.push(attr);
       else if (input[attr]) attributes.push(`${attr}="${input[attr]}"`);
