2bf51d34656acc5ef2ae0b4dae9ff9857c5e5934
Tino Vyatkin <tino@vtkn.io>
2018-12-09 12:38:44 -0400

    use validator

---
+++ __tests__/integration.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const amphtmlValidator = require('amphtml-validator');
+
+const Template = require('marko').load(
+  require.resolve('./templates/amp-full-render-doc.marko'),
+);
+
+describe('Full page render and validation', () => {
+  test('render and validate AMP version', async () => {
+    const render = await Template.render({
+      $global: {
+        amp: true,
+      },
+    }).toString();
+
+    expect(render).toMatchSnapshot();
+
+    // validate
+    const validator = await amphtmlValidator.getInstance();
+    const result = await validator.validateString(render);
+    expect(result.errors).toHaveLength(0);
+    expect(result).toHaveProperty('status', 'PASS');
+  });
+});
